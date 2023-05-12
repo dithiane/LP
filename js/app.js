efba80f2abed622e278f9e2acfc98fd58177c205
dithiane <elenamena.colorado@gmail.com>
2023-05-11 22:28:26 -0600

    Added Event Listener to Scroll

--- js/app.js
+++ js/app.js
@@ -4,21 +4,38 @@ const menu = document.querySelector(".menu");
 const sections = document.querySelectorAll('section');
 
 const toggleActive = (e) => {
+    e.preventDefault()
     hamburger.classList.toggle("active");
     menu.classList.toggle("active");
 }
 
-hamburger.addEventListener("click",toggleActive)
-
 const createMenu = () => {
-        sections.forEach(el => {
-            const title = el.getAttribute('id');
-            console.log(el.firstElementChild.innerText)
-            const menuItem = document.createElement('li');
-            menuItem.className = 'menu-item'
-            menuItem.innerHTML =  `<a class="menu-link" aria-controls="${el.id}" href="#">${el.firstElementChild.innerText}</a>`
-            menu.appendChild(menuItem)
-        });
+    sections.forEach(el => {
+        const menuItem = document.createElement('li');
+        menuItem.className = 'menu-item'
+        menuItem.innerHTML = `<a class="menu-link" href="#${el.id}">${el.firstElementChild.innerText}</a>`
+        menu.appendChild(menuItem)
+    });
 }
 
 createMenu()
+const isInViewPort = (el) => {
+    const rect = el.getBoundingClientRect();
+    return rect.top < window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2;
+};
+
+const handleScroll = (e) => {
+    e.preventDefault()
+    sections.forEach((el) => {
+        let menuItem = document.querySelector(`a[href="#${el.id}"]`);
+        if (isInViewPort(el)) {
+            el.classList.add('active');
+            menuItem.classList.add('active');
+        } else {
+            el.classList.remove('active');
+            menuItem.classList.remove('active');
+        }
+    });
+}
+window.addEventListener("scroll", handleScroll);
+hamburger.addEventListener("click", toggleActive)
